d0aa8d9627ea27dba159c2397d04d74ca1422caf
Nicolas5150 <Nicolas@Nicolas-MacBook-Pro.local>
2016-11-13 02:27:35 -0500

    Inclusion of other images and css
    Added thumbnail images into html as well as the arrays have been
    updated to include all full size images and corresponding captions. Css
    has also been finished to match styling of the original intended
    document.

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,53 @@
-function expandOne(event, imgNumber) {
+// Stores the location of all the images and captions globally
+var images = [
+    'img/flying-car-593.jpg',
+    "img/flying-car-m400.jpg",
+    "img/log200711a.jpg",
+    "img/flyingCar-article.jpg",
+    "img/saucercarSplash_450x325.jpg",
+    "img/high-road-aerocar.jpg"
+  ];
+var captions = [
+    'Flying Car 593',
+    "Flying Car M400",
+    "Plane and Car",
+    "Flying Plane Bottom",
+    "Flying Saucer",
+    "Flying Aerocar"
+  ];
+
+// This runs after the page finishes loading
+window.addEventListener('load',init,false);
+function init() {
+  var imgOneAnchor = document.getElementById('one');
+  // http://stackoverflow.com/questions/9643311/pass-string-parameter-in-an-onclick-function
+  imgOneAnchor.addEventListener('click',function(){
+    expandImage(event, 0);}
+      ,false);
+  var imgTwoAnchor = document.getElementById('two');
+  imgTwoAnchor.addEventListener('click',function(){
+    expandImage(event, 1);}
+      ,false);
+  var imgThreeAnchor = document.getElementById('three');
+  imgThreeAnchor.addEventListener('click',function(){
+    expandImage(event, 2);}
+      ,false);
+  var imgFourAnchor = document.getElementById('four');
+  imgFourAnchor.addEventListener('click',function(){
+    expandImage(event, 3);}
+      ,false);
+  var imgFiveAnchor = document.getElementById('five');
+  imgFiveAnchor.addEventListener('click',function(){
+    expandImage(event, 4);}
+      ,false);
+  var imgsixAnchor = document.getElementById('six');
+  imgsixAnchor.addEventListener('click',function(){
+    expandImage(event, 5);}
+      ,false);
+}
+
+// Load image that was clicked at full size
+function expandImage(event, imgNumber) {
   event.preventDefault();
 
   // Remove any items that are currently on the div tag at the moment
@@ -6,14 +55,18 @@ function expandOne(event, imgNumber) {
     imgHere.removeChild(imgHere.lastChild);
   }
 
-  // Obtain the image needed to show
-  var imgRequested = loadImgArray(imgNumber);
+  // Call the two functions to add the image and corresponding caption
+  createImage(images[imgNumber]);
+  createCaption(imgNumber);
+}
 
+// Create the image requested based off of index pos of images array passed in
+function createImage(imgRequested){
   // http://stackoverflow.com/questions/12287856/insert-image-object-into-html
   // Append the image to the imgHere div
   var img = new Image();
-  img.setAttribute("id","Aircraft");
-  img.setAttribute("alt","Aircraft Pic");
+  img.setAttribute("class","largeImage");
+  img.setAttribute("alt","Aircraft Picture");
   img.setAttribute("width", "auto");
   img.setAttribute("height", "auto");
   var pic = document.getElementById('imgHere');
@@ -21,26 +74,12 @@ function expandOne(event, imgNumber) {
   img.src = imgRequested;
 }
 
-// Stores the location of all the images
-// Each onclick will pass it a specific paramter
-// and that index coresponds to a certian image to return and load
-function loadImgArray(imgNumber) {
-  var images = [
-    'img/flying-car-593.jpg',
-    "Volvo",
-    "BMW"
-  ];
-
-  return images[imgNumber];
+// Create the caption requested based off of index pos of images array passed in
+// Append the text below the image as a caption
+function createCaption(imgNumber){
+  var para = document.createElement("p");
+  var node = document.createTextNode(captions[imgNumber]);
+  para.appendChild(node);
+  var element = document.getElementById("imgHere");
+  element.appendChild(para);
 }
-
-// This runs after the page finishes loading
-function init() {
-  var imgOneAnchor = document.getElementById('one');
-  // http://stackoverflow.com/questions/9643311/pass-string-parameter-in-an-onclick-function
-  imgOneAnchor.addEventListener('click',function(){
-    expandOne(event, 0);}
-      ,false);
-}
-
-window.addEventListener('load',init,false);
